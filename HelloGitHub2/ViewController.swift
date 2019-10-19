6d6719ea82b78def28caf1e2c246ce0943efc8cc
吉村勇作 <yoshimurayuusaku@yoshimurayuusakunoMacBook-Pro.local>
2019-10-19 16:11:05 +0900

    Intiital Commit

---
+++ HelloGitHub2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGitHub2
+//
+//  Created by 吉村勇作 on 2019/10/19.
+//  Copyright © 2019 yoshimurayusaku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloGitHub2Tests/HelloGitHub2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HelloGitHub2Tests.swift
+//  HelloGitHub2Tests
+//
+//  Created by 吉村勇作 on 2019/10/19.
+//  Copyright © 2019 yoshimurayusaku. All rights reserved.
+//
+
+import XCTest
+@testable import HelloGitHub2
+
+class HelloGitHub2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
